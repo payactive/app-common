6065f41b6b43e2d8bb252e19f19dfd6742c734ed
Finke, Andreas <andreas.finke@payactive.eu>
2020-12-09 21:31:56 +0000

    #111 Removes unused field

--- src/formik/index.js
+++ src/formik/index.js
@@ -1,4 +1,6 @@
 import InputField from './InputField';
 import CheckboxField from './CheckboxField';
 import SelectField from './SelectField';
-export { InputField, CheckboxField, SelectField };
+import SizedTextInputField from './SizedTextInputField';
+
+export { InputField, CheckboxField, SelectField, SizedTextInputField };
