6da6ff59e6886f8c216b66d19d34c1b587ee9685
Finke, Andreas <andreas.finke@payactive.eu>
2020-12-10 00:38:21 +0000

    Try to move files to root dir

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import InputField from './InputField';
-import CheckboxField from './CheckboxField';
-import SelectField from './SelectField';
-import SizedTextInputField from './SizedTextInputField';
-
-export { InputField, CheckboxField, SelectField, SizedTextInputField };
