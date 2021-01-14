9f4963448d635ea8adb7463e6ec54d9f43625f40
Finke, Andreas <andreas.finke@payactive.eu>
2021-01-14 16:51:17 +0000

    Adds missing export

--- src/formik/index.tsx
+++ src/formik/index.tsx
@@ -1,6 +1,7 @@
+import FormikForm from './FormikForm';
 import FormikInputField from './FormikInputField';
 import FormikSelectField from './FormikSelectField';
 import FormikSizedInputField from './FormikSizedInputField';
 
-export { FormikInputField, FormikSelectField, FormikSizedInputField };
+export { FormikForm, FormikInputField, FormikSelectField, FormikSizedInputField };
 
